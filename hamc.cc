304f4c84940b7696067ca1bdbdbdfa6b74a86599
t_ohta <phjgt308@ybb.ne.jp>
2010-07-25 00:09:52 +0900

    コード整理:

--- hamc.cc
+++ hamc.cc
@@ -15,38 +15,58 @@ unsigned parse_hex(const char* s) {
 
 int main(int argc, char** argv) {
   if(argc != 2 && argc != 3) {
+  ARG_ERROR:
     std::cerr << "Usage: hamc [--lower-frequency-limit=2] <model-index>" << std::endl;
     return 1;
   }
-
+  
+  // parse arguments
   unsigned lower_frequency_limit=2;
-  unsigned arg_i=1;
+  int arg_i=1;
   if(strncmp(argv[arg_i], OPT1, strlen(OPT1))==0)
     lower_frequency_limit = atoi(argv[arg_i++]+strlen(OPT1));
-  const char* index_filepath=argv[arg_i];
 
+  if(arg_i == argc)
+    goto ARG_ERROR;  // too few arguments
+
+  const char* index_filepath=argv[arg_i];
+  
+  // parse model file
   HAM::Trie::Builder::KeyList keys;
   std::string line;
 
   unsigned total_ham_count=0;  
   unsigned total_spam_count=0;
   
+  std::cerr << "= Read model definition:" << std::endl;
+
+  // first line
   if(std::getline(std::cin, line)) {
     total_ham_count  = parse_hex(line.c_str());
     total_spam_count = parse_hex(line.c_str()+9);
   }
+  std::cerr << "  == HAM count : " << total_ham_count 
+	    << "  == SPAM count: " << total_spam_count << std::endl;
 
+  // rest lines
+  Builder::ProbabilityCalculator pc(total_ham_count, total_spam_count);
   while(std::getline(std::cin, line)) {
     const char* s = line.c_str();
     unsigned ham_count  = parse_hex(s);
     unsigned spam_count = parse_hex(s+9);
     if(ham_count + spam_count >= lower_frequency_limit)
-       keys.push_back(Builder::Key(s+18, ham_count, spam_count, total_ham_count, total_spam_count));
+      keys.push_back(Builder::Key(s+18, pc.bayesian_spam_probability(ham_count, spam_count)));
   }
   std::sort(keys.begin(), keys.end());
   
+  // convert to trie format and save
   HAM::Trie::Builder bld(keys);
-  bld.build().save(index_filepath);
+  std::cerr << "= Build trie index: " << std::endl;
+  bld.build();
   
+  std::cerr << "= Save: " << index_filepath << std::endl;
+  bld.save(index_filepath);
+  
+  std::cerr << "DONE" << std::endl;
   return 0;
 }
